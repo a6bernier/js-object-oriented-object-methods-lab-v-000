0303f12347c56f22d81f302716b3c7b85752fab3
Andrew Bernier <a6bernier@gmail.com>
2018-12-18 22:36:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function BoardMember (name, homeState, training){
     return `Your will see great things from Scuber`
   }
 
-  this.sayHi(){
+  this.sayHi = function {
     return `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`
   }
 }
