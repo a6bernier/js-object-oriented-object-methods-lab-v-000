57e79f4cae49e737a34e86d474f6565880055f45
Andrew Bernier <a6bernier@gmail.com>
2018-12-18 22:35:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,18 +2,23 @@ function BoardMember (name, homeState, training){
   this.name = name
   this.homeState = homeState
   this.training = training
+  
   this.veto = function(){
     return `No, I must disagree`
   }
+  
   this.approve(){
     return `You can do that!`
   }
+  
   this.doCharity(){
     return `I like to help people`
   }
+  
   this.releasePressStatement(){
     return `Your will see great things from Scuber`
   }
+  
   this.sayHi(){
     return `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`
   }
